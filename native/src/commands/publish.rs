0e3613e9d77e556edc84ebc01d8288add467ec4e
Terence Lee <hone02@gmail.com>
2017-09-26 23:36:41 -0500

    workaround midgard encoding issues for publish command

--- native/src/commands/publish.rs
+++ native/src/commands/publish.rs
@@ -3,7 +3,7 @@ extern crate reqwest;
 extern crate serde;
 
 use heroku_api::HerokuApi;
-use self::percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
+//use self::percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
 use self::reqwest::StatusCode;
 
 #[derive(Serialize, Deserialize)]
@@ -28,26 +28,49 @@ pub struct Publish {
 impl Publish {
     pub fn execute(self) {
         let api = HerokuApi::new();
-        let body = json!(CreateRevisions::new(&self.tag));
-        let buildpack = format!("{}/{}", &self.namespace, &self.name);
-        let url_path = format!("/buildpacks/{}/revisions", utf8_percent_encode(&buildpack, PATH_SEGMENT_ENCODE_SET).to_string());
-        let response = api.post_with_version(&url_path, "3.buildpack-registry", body).unwrap();
-
+        let response = &api.get_with_version("/buildpacks", "3.buildpack-registry").unwrap();
         match response.status {
             StatusCode::Ok => {
-                let json = response.body;
-                let name = json["name"].as_str().unwrap();
-                println!("Successfully registered buildpack '{}'.", name);
-            },
-            StatusCode::UnprocessableEntity => {
-                let json = response.body;
-                for entry in json.as_object().unwrap() {
-                    let (key, value) = entry;
-                    let error_message = value.as_array().unwrap().into_iter().map(|item| item.as_str().unwrap()).collect::<Vec<&str>>().join("\n");
-                    println!("{} {}", key, error_message);
+                let json = &response.body;
+                let buildpack = json.as_array().unwrap().into_iter().find(|buildpack| {
+                    buildpack["namespace"] == self.namespace && buildpack["name"] == self.name
+                });
+                match buildpack {
+                    Some(buildpack) => {
+                        let uuid = buildpack["id"].as_str().unwrap();
+                        let body = json!(CreateRevisions::new(&self.tag));
+                        let url_path = format!("/buildpacks/{}/revisions", uuid);
+                        let response = api.post_with_version(&url_path, "3.buildpack-registry", body).unwrap();
+                        match response.status {
+                            StatusCode::Ok => {
+                                let json = response.body;
+                                let s3_url = json["s3_http_url"].as_str().unwrap();
+                                println!("Successfully published buildpack '{}/{}' for {}.\n{}", self.namespace, self.name, self.tag, s3_url);
+                            },
+                            StatusCode::UnprocessableEntity => {
+                                let json = response.body;
+                                for entry in json.as_object().unwrap() {
+                                    let (key, value) = entry;
+                                    let error_message = match value.as_array() {
+                                        Some(array) => {
+                                            array.into_iter().map(|item| item.as_str().unwrap()).collect::<Vec<&str>>().join("\n")
+                                        },
+                                        None => value.as_str().unwrap().to_string(),
+                                    };
+                                    println!("{}: {}", key, error_message);
+                                }
+                            },
+                            status => println!("Could not publish buildpack.\nReceived: {}, {}", status, response.body),
+                        }
+
+                    },
+                    None => {
+                        println!("No buildpack found with that namespace/name: {}/{}", self.namespace, self.name)
+                    }
                 }
-            },
-            status => println!("Could not register buildpack.\nReceived: {}, {}", status, response.body),
-        }
+            }
+            status => println!("Could not get buildpack uuid.\nReceived: {}, {}", status, response.body),
+        };
+
     }
 }
