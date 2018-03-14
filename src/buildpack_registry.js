514fdf488492388d64426abc191594a1847d7efc
Terence Lee <hone02@gmail.com>
2018-03-13 20:27:24 -0500

    buildpacks:add and buildpacks:set use the registry

---
+++ src/buildpack_registry.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import HTTP from 'http-call'
+
+export default class BuildpackRegistry {
+  static options = {
+      method: 'GET',
+      protocol: 'https:',
+      path: '/',
+      raw: false,
+      partial: false
+  }
+
+  static url() {
+    if (process.env.HEROKU_BUILDPACK_REGISTRY_URL === undefined) {
+      return 'buildpack-registry.herokuapp.com';
+    } else {
+      return process.env.HEROKU_BUILDPACK_REGISTRY_URL;
+    }
+  }
+
+  static headers(token) {
+    let defaultHeaders = {
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/vnd.heroku+json; version=3.buildpack-registry',
+      'Content-Type': 'application/json'
+    };
+
+    if (process.env.HEROKU_HEADERS) {
+      return Object.assign({}, defaultHeaders, JSON.parse(process.env.HEROKU_HEADERS))
+    } else {
+      return defaultHeaders;
+    }
+  }
+
+  static create(token) {
+    return HTTP.create(Object.assign(this.options, {host: this.url()}, {headers: this.headers(token)}))
+  }
+}
